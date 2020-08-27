c6d005dd56f5965f6817e5aab5f8ba18d619ac0d
김승기 <skkim0832@naver.com>
2020-08-27 14:39:23 +0900

    2시반까지 한 가벼운 워밍업2

--- Algo/src/backjoon_backup/bj_1316.java
+++ Algo/src/backjoon_backup/bj_1316.java
@@ -1,4 +1,4 @@
-package backjoon;
+package backjoon_backup;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+public class bj_1316 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
