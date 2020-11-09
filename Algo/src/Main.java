cf02c72f972854d65e9617507ef9667c7329d50f
김승기 <skkim0832@naver.com>
2020-11-09 09:20:16 +0900

    push

--- Algo/src/Main.java
+++ Algo/src/Main.java
@@ -3,36 +3,35 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st = null;
 	static StringBuilder sb = new StringBuilder();
+	static int N;
 	static int[] arr;
-	static boolean[] visit = new boolean[1000001];
+	static int[] min;
+	static int count=0;
+	static StringTokenizer st = null;
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		arr = new int[N+1];
-		arr[0]=0;
-		arr[1]=0;
-
-		for(int i=2;i<=N;i++) {
-			arr[i]=arr[i-1]+1;
-			if(i%2==0 && arr[i/2]+1<arr[i]) {
-				arr[i]=arr[i/2]+1;
-			}
-			if(i%3==0 && arr[i/3]+1<arr[i]) {
-				arr[i]=arr[i/3]+1;
-			}
-
+		N = Integer.parseInt(st.nextToken());
+		arr =new int[N+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
+		dp();
 		bw.write(arr[N]+"\n");
-        bw.flush();
+		bw.flush();
 		bw.close();
 	}
-
+	private static void dp() {
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				arr[i]=Math.min(arr[i], arr[j]+arr[i-j]);
+			}
+		}
+	}
 }
