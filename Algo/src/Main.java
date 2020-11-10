96f0f782d2a299acaf08825aa6db6a75200ebc52
김승기 <skkim0832@naver.com>
2020-11-10 13:50:06 +0900

    push

--- Algo/src/Main.java
+++ Algo/src/Main.java
@@ -10,28 +10,32 @@ class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb = new StringBuilder();
 	static int N;
-	static int[] arr;
-	static int[] min;
-	static int count=0;
+	static long[][] arr;
 	static StringTokenizer st = null;
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		arr =new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1;i<=N;i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+		arr = new long[N+1][10];
+			for(int i=1;i<=9;i++) {
+				arr[1][i]=1;
+			}
+			for(int i=2;i<=N;i++) {
+				for(int j=0;j<=9;j++) {
+					if(j==0) {
+						arr[i][j]=(arr[i-1][j+1])%1000000000;
+					}else if(j==9) {
+						arr[i][j]=(arr[i-1][j-1])%1000000000;
+					}else {
+						arr[i][j]=(arr[i-1][j-1]+arr[i-1][j+1])%1000000000;
+					}
+				}
+			}
+		long value=0;
+		for(int i=0;i<10;i++) {
+			value+=arr[N][i];
 		}
-		dp();
-		bw.write(arr[N]+"\n");
+		bw.write(value%1000000000+"\n");
 		bw.flush();
 		bw.close();
-	}
-	private static void dp() {
-		for(int i=1;i<=N;i++) {
-			for(int j=1;j<=i;j++) {
-				arr[i]=Math.min(arr[i], arr[j]+arr[i-j]);
-			}
-		}
 	}
 }
