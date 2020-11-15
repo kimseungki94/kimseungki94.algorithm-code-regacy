c023d30a642d60cefeb225e93e80315216e74c9b
“kimseungki94” <skkim0832@naver.com>
2020-11-16 01:03:22 +0900

    push

--- Algo/src/Main.java
+++ Algo/src/Main.java
@@ -13,20 +13,11 @@ class Main {
 	static long[] arr;
 	static StringTokenizer st = null;
 	public static void main(String[] args) throws IOException {
+		System.out.println("dfsd");
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		arr = new long[1000001];
-		arr[0]=0;
-		arr[1]=1;
-		arr[2]=2;
-		arr[3]=4;
-		for(int i=4;i<=1000000;i++) {
-			arr[i]=(arr[i-3]+arr[i-2]+arr[i-1])%1000000009;
-		}
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			sb.append(arr[Integer.parseInt(st.nextToken())]+"\n");
-		}
+
 		bw.write(sb+"\n");
 		bw.flush();
 		bw.close();
